b5ed07ee1435e6c8a1a826635967d7dd0125a8c1
segun olowoyeye <segunolowoyeye@gmail.com>
2024-01-15 23:32:49 +0100

    readded auth

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import { useForm, ValidationError } from '@formspree/react';
 import '../style/Contact.css';
 
 const Contact = () => {
+  const [state, handleSubmit] = useForm("mjvnkeqy");
+  if (state.succeeded) {
+      return <p>Thanks for joining!</p>;
+  }
   return (
     <div className='contact' id='contact'>
         <div className='infopart'>
@@ -9,14 +14,29 @@ const Contact = () => {
       <p>I would love to hear about your project and how I could help. Please fill the form, and I'll get back to you as soon as possible.</p>
     </div>
     <div className='formf'>
-    <form action="https://formspree.io/f/mjvnkeqy" method="post">
+     <form onSubmit={handleSubmit}> 
     <label for="name">Name:</label>
     <input type="text" id="name" name="name" required />
+    <ValidationError 
+        prefix="Name" 
+        field="name"
+        errors={state.errors}
+      />
     <label for="name">Email:</label>
     <input type="email" id="email" name="email" required />
+    <ValidationError 
+        prefix="Email" 
+        field="email"
+        errors={state.errors}
+      />
     <label for="message">Message:</label>
     <textarea id="message" name="message" rows="4" required></textarea>
-    <button type="submit">Send Message</button>
+    <ValidationError 
+        prefix="Message" 
+        field="message"
+        errors={state.errors}
+      />
+    <button type="submit" disabled={state.submitting}>Send Message</button>
     </form>
     </div>
     </div>
